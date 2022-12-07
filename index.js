84c35cdd5aca224e838ee2f3ae400e30cf5b81d7
dlsxody1 <dlsxody1@naver.com>
2022-12-07 17:43:20 +0900

    테이블 셀 병합을 이해해보자

--- index.js
+++ index.js
@@ -28,20 +28,60 @@ $(function () {
       tableUpperTag += "<tr/>";
     }
     tableUpperTag += "</table>";
+    tableUpperTag += `<div class='add_row_hidden' contenteditable='false' data-row=${count}>행 추가하기</div> `;
 
-    $(".table_insert").append(tableUpperTag);
-    $(".table_box")
-      .last()
-      .append(
-        `<div class="add_column_hidden"><button name="btnAddColumn" data-col="${count}">okok</button></div>`
-      );
-    $(".table_insert")
-      .last()
-      .append(
-        `<div class="add_row_hidden"><button name="btnAddRow" data-row="${count}">++++</button></div>
-                                
-                `
-      );
+    if ($(".editor_container div").length === 0) {
+      console.log("ho");
+      $(".editor_container div").prepend(tableUpperTag);
+      $(".editor_container").append("<div>\u00a0 </div>");
+    } else {
+      $(".editor_container div").last().append(tableUpperTag);
+      $(".editor_container").append(`<div>\u00a0</div>`);
+    }
   };
-  console.log($("img").data("center"));
+
+  $(".table_logo").on("click", function () {
+    makeTable();
+  });
+
+  $(".lili").on("dragstart", function (e) {
+    $(this).addClass("dragging");
+  });
+
+  $(".lili").on("dragend", function (e) {
+    $(this).removeClass("dragging");
+  });
+
+  /**
+   * [x] 엘리먼트의 .draggable, .container의 배열로 선택자를 지정합니다.
+   * [x] draggables를 전체를 루프하면서 dragstart, dragend를 이벤트를 발생시킵니다.
+   * [x] dragstart, dragend 이벤트를 발생할때 .dragging라는 클래스를 토글시킨다.
+   * [x] dragover 이벤트가 발생하는 동안 마우스 드래그하고 마지막 위치해놓은 Element를 리턴하는 함수를 만듭니다.
+   */
+});
+
+// 테이블 아래 버튼은 숨어있다가 버튼쪽에 마우스 커서가 가면
+// 로우를 추가 할 수 있는 버튼이 나온다.
+$(document).on("mouseenter", ".add_row_hidden", function () {
+  console.log("mouseIn");
+  $(".add_row_hidden")
+    .removeClass("add_row_hidden")
+    .addClass("add_row_onmouse");
+});
+
+$(document).on("mouseout", ".add_row_onmouse", function () {
+  $(".add_row_onmouse")
+    .removeClass("add_row_onmouse")
+    .addClass("add_row_hidden");
+});
+
+// 행 추가 버튼 이벤트
+
+$(document).on("click", ".add_row_onmouse", function (event) {
+  let row = $(this).data("row");
+  //현재 누른 table에 tr이 몇개인지 세야함.
+  console.log("");
+  let addblock = "<tr>";
+
+  console.log(row);
 });
